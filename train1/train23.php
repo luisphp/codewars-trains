d42aeb8c9269be7ca237ab690ce261969cd295cb
Luis Hurtado <luis.hurtado@linio.com>
2020-04-02 20:38:59 -0400

    More items

--- train1/train23.php
+++ train1/train23.php
@@ -12,21 +12,11 @@ Given a string, detect whether or not it is a pangram. Return True if it is, Fal
 
 */
 
-var_dump(solve("khrushchev"));
+var_dump(solve("The quick brown fox jumps over the lazy dog 1 ?"));
 
-function solve($s) {    
-    $papas = ["a","e","i","o","u"];
-    $arequipe = str_split($s);
-    $mantequilla = 0;
-    $bolsa = array();
-    $sabores = ["a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" =>7, "h" =>8, "i" => 9, "j" => 10, "k" => 11, "l" => 12, "m" => 13, "n" => 14, "o" => 15, "p" => 16, "q" => 17, "r" => 18, "s" => 19, "t" => 20, "u" => 21,"v" => 22, "w" => 23, "x" => 24, "y" => 25, "z" => 26];
-    for ($i=0; $i < count($arequipe); $i++) { 
-        if(!in_array($arequipe[$i], $papas)){
-            $mantequilla =  $mantequilla + $sabores[$arequipe[$i]];
-        }else{
-            array_push($bolsa, $mantequilla);
-            $mantequilla = 0;
-        }
-    }
-    return max($bolsa);
+function solve($string) { 
+    $limpio = preg_replace("/[^A-Za-z]/", '', strtolower(str_replace(' ','',$string)));
+    $palabras = array_unique(str_split($limpio));
+    count($palabras) == 26?  $palabras= true: $palabras = false;
+    return $palabras;
 }
